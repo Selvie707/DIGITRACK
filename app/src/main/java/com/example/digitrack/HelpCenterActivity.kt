3582a1715d8dc6d790aaee6bd29e783418c45921
Selvie707 <113976495+Selvie707@users.noreply.github.com>
2024-03-24 05:36:12 +0700

    XML RAW
    SCHEDULE, HELP CENTER, ATTENDANCE, DAILY REPORT, MATERIALS, DETAIL STUDENT, ALERT DIALOG SCHEDULE

---
+++ app/src/main/java/com/example/digitrack/HelpCenterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.digitrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelpCenterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_center)
+    }
+}
