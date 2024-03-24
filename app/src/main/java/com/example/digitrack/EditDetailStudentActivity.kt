5a421c135661825cc058fad86e42a0a587014155
MuliaSahpira <muliasahpira@gmail.com>
2024-03-24 21:25:09 +0700

    XML Home, Add new Student, Edit student

---
+++ app/src/main/java/com/example/digitrack/EditDetailStudentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.digitrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDetailStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_detail_student)
+    }
+}
